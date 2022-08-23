8b608df58845014e90cefb0f3cbc9dbdd69e9370
YuriKha <yurikhazanov1@gmail.com>
2022-08-23 13:41:21 +0300

    change my port to 5656 from 3000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http'); // חיבור לספרייה
 const app=require('./app'); //   ייבוא של קובץ אפ ,יש לייצא קובץ אפ בתוך אפ על מנת שנוכל לייבא
 
 const server=http.createServer(app);
-const port=3000; // המשתנה פורט יכיל מספר הפורט
+const port=5656; // המשתנה פורט יכיל מספר הפורט
 server.listen(port,()=>{ // אומר לשרת להתחיל להאזין לפורט
     console.log('you started the server') // ולהדפיס שהשרת התחיל
 }); 
