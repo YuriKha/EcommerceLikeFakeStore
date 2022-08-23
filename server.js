d87502b67accf55111e6781980f54febad71ad02
YuriKha <yurikhazanov1@gmail.com>
2022-08-23 14:01:18 +0300

    add a defult port in server.js file and add it in my .env file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http'); // חיבור לספרייה
 const app=require('./app'); //   ייבוא של קובץ אפ ,יש לייצא קובץ אפ בתוך אפ על מנת שנוכל לייבא
 
 const server=http.createServer(app);
-const port=process.env.PORT || 5656; // המשתנה פורט יכיל מספר הפורט
+const port=process.env.PORT || 5656; // המשתנה פורט יכיל מספר הפורט ובמידה והוא תפוס יהיה חליפי
 server.listen(port,()=>{ // אומר לשרת להתחיל להאזין לפורט
     console.log('you started the server') // ולהדפיס שהשרת התחיל
 }); 
